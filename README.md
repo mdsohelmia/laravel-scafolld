600354d4c1ba67dc2cd6f285c510af2f2c2131c6
Sohel Mia <sohelcse1999@gmail.com>
2021-12-02 20:51:48 +0600

    Install laravel,inertiajs,Vuejs,Tailwindcss,headlessui

---
+++ README.md
@@ -0,0 +1,1 @@
+# Build single-page apps, without building an API.
---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+window._ = require('lodash');
+import {createApp, h} from 'vue'
+import {createInertiaApp} from '@inertiajs/inertia-vue3'
+import {InertiaProgress} from '@inertiajs/progress'
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .mount(el)
+    },
+}).then(r => {});
+
+InertiaProgress.init()
